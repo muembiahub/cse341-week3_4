1d578b8eae7e7837f75d4c14e25e13b8e06ae35d
Muembia <jonathanmuembia3@gmail.com>
2026-05-30 18:14:08 +0200

    Fix passport production callback url paths

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
-const User = require('../models/User'); 
+const User = require('../models/user'); 
 
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
